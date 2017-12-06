b42175d6d1840fc49468bdc3e4fadde73eb9c822
scrumpyjack <scrumpyjack@st.ilet.to>
2017-12-06 22:03:50 +0000

    Initial commit

--- weesms.py
+++ weesms.py
@@ -2,53 +2,7 @@
 from __future__ import unicode_literals 
 import weechat, csv, re, gammu, time, unicodedata
 
-"""
-README
-======
 
-2015-04-12: v0.1 - Initial/draft/beta version
-
-Installation
-------------
-
-$ sudo aptitude install gammu gammu-smsd python-gammu
-$ sudo usermod -a -G dialout $USER
-
-$ cp weesms.py ~/.weechat/python/[autoload]
-
-Stop gammu-smsd if it is started:
-# /etc/init.d/gammu-smsd stop
-
-
-Plugin management
------------------
-
-Load plugin:
-/python load python/weesms.py or put the script in python/autoload
-
-Reload plugin:
-/python reload weesms
-
-Unload plugin:
-/python unload weesms
-
-
-Phonebook
----------
-
-Default CSV phonebook location: WEECHAT_DIR/python/weesms.csv
-
-Phonebook format is:
-bob,+33123456789
-alice,+33987654321
-
-
-Refs:
-  - http://wammu.eu/docs/pdf/gammu.pdf
-  - https://weechat.org/files/doc/stable/weechat_scripting.en.html
-  - https://weechat.org/files/doc/stable/weechat_plugin_api.en.html
-
-"""
 
 SCRIPT_NAME    = "weesms"
 SCRIPT_AUTHOR  = "Steeve Barbeau"
